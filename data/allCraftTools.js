5e51fc121fcea37ce4fbed7e2fb79237a69d42ae
Hollis Vohr <hollis.vohr@gmail.com>
2023-10-02 09:10:54 -0600

    fix: refactor data sets to mimic eachother

--- data/allCraftTools.js
+++ data/allCraftTools.js
@@ -1,6 +1,9 @@
 const allCraftTools = [
   {
     name: 'Yarn Ball Winder',
+    brand: 'Darn Good Yarn',
+    category: 'tools',
+    color: null,
     id: 80,
     price: '$35.22',
     details: 'This yarn ball winder features easy assembly instructions and can be disassembled to make traveling easier! Never cake a skein of yarn by hand again!',
@@ -10,7 +13,10 @@ const allCraftTools = [
   },
   {
     name: 'Purple Cosmic Ceramic Yarn Bowl',
+    brand: 'The Knit Nest',
     id: 90,
+    color: null,
+    category: 'tools',
     price: '$30.05',
     details: 'Color: Purple Speckled Size: 5.5"W x 4"H 100% Lead-Free',
     description: 'Meet our ceramic yarn bowl - your crafty side/s new best friend! Whether you/re deep into knitting or big on crocheting, this bowl keeps your yarn neat and tangle-free.Swap yarn chaos for pure crafting bliss with this stylish ceramic bowl!',
@@ -19,7 +25,10 @@ const allCraftTools = [
   },
   {
     name: 'Aluminum Crochet Hook Set',
+    brand: 'Darn Good Yarn',
     id: 100,
+    color: null,
+    category: 'tools',
     price: '$25.00',
     details: 'Colored Aluminum Hooks Sizes: 2.0mm (B/1), 2.5mm (C/2), 3.0mm (D/3), 3.5mm (E/4), 4.0mm (G/6), 4.5mm(7), (H/8) 5.0mm (H/9), 5.5mm (I/9), 6.0mm (J/10), 6.5mm (K/10.5)Silver Tone Hooks Sizes:.50mm, .75mm, .85mm, .90mm, 1.0mm, 1.10mm, 1.40mm, 1.50mm, 1.60mm, 1.75mm, 1.80mm, 1.90mm. That/s 22 crochet hooks total! Not to mention each pack comes with a purple carrying case.',
     description: 'This crochet hook pack comes with all the hooks you/ve been wanting! Never be caught without the right hook when inspiration strikes with this expansive set. It also comes with a handy case to keep those hooks organized and portable',
