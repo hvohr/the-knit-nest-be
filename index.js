55538958a33738746b98d452ea57169b6c4ba278
Hollis Vohr <hollis.vohr@gmail.com>
2023-10-07 13:01:47 -0600

    feat: add post request

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 // const knex = require("./knex");
 const allProducts = require('./data/allProducts')
 const products = require('./data/products')
-const users = require('./data/users')
 
 app.set('port', process.env.PORT || 3001);
 app.use(express.json());
@@ -13,20 +12,15 @@ app.use(cors());
 app.locals.title = "The Knit Nest"
 app.locals.allproducts = allProducts
 app.locals.products = products
-app.locals.users = users
-
-
-// app.get("/api/v1/:category", async (request, response) => {
-//   const category = request.params.category
-//   console.log(category)
-//   const inputCategory = await knex.from("allProducts").select("category").where({ category: category })
-//   if (inputCategory.length) {
-//     const foundProducts = await knex.from("allProducts").select().where({ category: category })
-//     response.status(200).json(foundProducts);
-//   } else {
-//     res.sendStatus(404);
-//   }
-// });
+app.locals.users = [
+  {
+    userID: 1,
+    name: "Hollis Vohr",
+    email: "hollis.vohr@gmail.com",
+    password: "sadie",
+    cart: []
+  }
+];
 
 app.get("/api/v1/products/:category", async (request, response) => {
   let category = request.params.category
@@ -43,8 +37,14 @@ app.get("/api/v1/products", async (request, response) => {
 })
 
 app.get("/api/v1/users", async (request, response) => {
-  response.status(200).json({ users: app.locals.users });
+  return response.status(200).json({ users: app.locals.users });
 })
+
+app.post('/api/v1/users', (request, response) => {
+  app.locals.users.push(request.body)
+  response.status(201).json(app.locals.users);
+})
+
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
