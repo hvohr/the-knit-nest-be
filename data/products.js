ea9e3db741a45bf5aa6712c2ef3dbbe705217623
Hollis Vohr <hollis.vohr@gmail.com>
2023-10-12 15:18:38 -0600

    fix: small issue with spelling in descriptions

--- data/products.js
+++ data/products.js
@@ -8,7 +8,7 @@ const products = [
     color: null,
     yarnWeight: null,
     price: '$21.99',
-    details: "Type: Paperback, ISBN: 9781645679356, Publisher: Page Street Publishing publicationDate: 11/13/2023, Pages: 144, Product Dimensions: 7.00(w) x 9.00(h) x 1.00(d)",
+    details: "Type: Paperback, ISBN: 9781645679356, Publisher: Page Street Publishing, Publication Date: 11/13/2023, Pages: 144, Product Dimensions: 7.00(w) x 9.00(h) x 1.00(d)",
     description: 'Crochet charming knotted loveys with this easy guide to amigurumi from Alyson Dratch and Lindsay McNutt, the designers and mothers behind Mama Made Minis. From snuggly farmyard friends like Lettie Lamb and CeeCee Chick to magical creatures like Una Unicorn and Dusty Dragon, these 16 patterns are sure to bring comfort and joy to your minis every day. Thanks to the heirloom quality of these designs, you can create each stitch with love, knowing that your child/s new plushie will be by their side for years to come!',
     image: "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B600%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&set=key%5Bresolve.format%5D,value%5Bwebp%5D&source=url%5Bhttps://prodimage.images-bn.com/pimages/9781645679356_p0_v3_s600x595.jpg%5D&scale=options%5Blimit%5D,size%5B600x10000%5D&sink=format%5Bwebp%5D",
   },
